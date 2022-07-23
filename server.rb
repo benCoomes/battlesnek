a68d2f5f3d43dcf490c608dacce7f9f81e6fed3d
Ben Coomes <benCoomes@github.com>
2022-07-23 17:25:17 -0400

    basic battlesnake endpoints

--- server.rb
+++ server.rb
@@ -1,5 +1,31 @@
 require 'sinatra'
 
 get '/' do
-  'Hola, mundo!'
+<<-SNEKMETA
+{
+  "apiversion": "1",
+  "author": "benCoomes",
+  "color": "#F56600",
+  "head": "tiger-king",
+  "tail": "tiger-tail",
+  "version": "0.0.1"
+}
+SNEKMETA
+end
+
+post '/start' do
+  200
+end
+
+post '/move' do
+<<-SNEKMOVE
+{
+ "move": "right",
+ "shout": "This seems like the right move!"
+}
+SNEKMOVE
+end
+
+post '/end' do
+  200
 end
